7e74a626535b5e9bda34afd97adf754144d33def
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-30 13:29:05 +0100

    Initial commit

---
+++ src/main/java/net/jazflix/springboot3mediaapi/Springboot3MediaApiApplication.java
@@ -0,0 +1,13 @@
+package net.jazflix.springboot3mediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3MediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3MediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jazflix/springboot3mediaapi/Springboot3MediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jazflix.springboot3mediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3MediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
